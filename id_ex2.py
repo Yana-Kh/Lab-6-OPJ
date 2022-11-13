8e35091be95f0c5dceb80cc48dc422cda3c022f2
Yana-Kh <ynakh2017@mail.ru>
2022-11-13 22:24:02 +0300

    ex

---
+++ id_ex2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+
+if __name__ == '__main__':
+    s = input("Введите число: ")
+    n = 0
+    if s.isdigit():
+        for i in s:
+            n += int(i)
+    else:
+        print("Обнаружены не числовые символы", file=sys.stderr)
+        exit(1)
+    print(f"Сумма цифр числа {s}: {n}")
